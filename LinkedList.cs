e0a362cd3554f735189b35743ccf4be56d8932a7
Mohammed Abdullah <mohammed.aladhary@outlook.com>
2023-07-09 11:29:22 +0400

    insert and display linked list

---
+++ LinkedList.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linkedList
+{
+    internal class LinkedList
+    {
+        private Node head;
+       public LinkedList() 
+        {
+            head = null;
+        }
+
+        public void insert(int data)
+        {
+            Node newNode = new Node(data);
+
+            if(head == null)
+            {
+                head = newNode;
+            }
+            else
+            {
+                Node currentNode = head;
+                while(currentNode.next != null)
+                {
+                    currentNode = currentNode.next;
+                }
+                currentNode.next = newNode;
+            }
+        }
+        public void display()
+        {
+            Node node = head;   
+            while(node != null)
+            {
+                Console.Write(node.data + " "); //prints them into a raw
+                //Console.WriteLine(node.data);   //prints them into a column
+                node = node.next;
+            }
+        }
+        public void remove(int data)
+        {
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace linkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            LinkedList list = new LinkedList();
+            list.insert(10);
+            list.insert(20);
+            list.insert(30);
+            list.insert(40);
+            Console.WriteLine();
+            Console.WriteLine("Original list:");
+            list.display();
+
+        }
+    }
+}
